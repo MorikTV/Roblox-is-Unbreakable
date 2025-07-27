95ced3c8f3ecc965aa3a22df2f4afce26897e93b
MorikTV <110910764+MorikTV@users.noreply.github.com>
2025-07-26 22:36:19 -0700

    Update Unbreakable.lua

--- Unbreakable.lua
+++ Unbreakable.lua
@@ -4,6 +4,7 @@
 
 local syn = getgenv().syn
 getgenv().syn = false
+
 loadstring(game:HttpGet("https://raw.githubusercontent.com/MorikTV/Roblox-is-Unbreakable/refs/heads/main/source.lua", true))()
 
 getgenv().syn = syn
