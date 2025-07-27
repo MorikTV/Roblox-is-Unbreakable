45bcff19436cd2c2066ef4c5bfb8bed862348ff1
MorikTV <110910764+MorikTV@users.noreply.github.com>
2025-07-26 22:36:11 -0700

    Update Unbreakable.lua

--- Unbreakable.lua
+++ Unbreakable.lua
@@ -1,6 +1,11 @@
+-- Roblox is Unbreakable script by morikyt (discord)
+-- DISCORD SERVER https://discord.gg/xsjEY7Xw7e
+-- Last Updated (February 9, 2025)
 
 local syn = getgenv().syn
 getgenv().syn = false
 loadstring(game:HttpGet("https://raw.githubusercontent.com/MorikTV/Roblox-is-Unbreakable/refs/heads/main/source.lua", true))()
 
 getgenv().syn = syn
+
+-- end
